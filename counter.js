724b50b1f4a59dc56939f11a763185617d185dee
Faried-4A6A <514152@edu.rocmn.nl>
2017-05-24 23:38:03 +0200

    done

---
+++ counter.js
@@ -0,0 +1,21 @@
+function showOnce(){
+    console.log(setTimeout(showOnce, 10));
+                            
+    var a = document.getElementById("mbody");
+            a.innerHTML = counter;
+                            
+}
+setTimeout(showOnce, 10);
+
+var counter = 0;
+var setIntervalId = setInterval(function(){
+
+    counter += 1;
+    if(counter == 100){
+        clearInterval(setIntervalId);
+        document.getElementById("home").style.backgroundColor = 'black';
+        document.getElementById("mbody").style.backgroundColor = 'white';
+        document.getElementById("mbody").style.color = 'black';
+
+    }
+},100);
